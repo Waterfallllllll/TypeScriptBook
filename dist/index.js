29f73f657cef39541cb907d86268a768eb836729
Ilya <ilieturcan424@gmail.com>
2026-09-05 21:20:44 +0300

    Перегрузка типа функции

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+function calculateTax(amount) {
+    if (amount != null) {
+        return amount * 1.2;
+    }
+    return null;
+}
+function writeValue(label, value) {
+    console.log(`${label}: ${value}`);
+}
+let taxAmount = calculateTax(100);
+// if (typeof taxAmount === "number") {
+//     writeValue("Tax value", taxAmount);
+// }
