4a4aa8a45c8985830018e5000c0bbd013b1007ce
Ilya <ilieturcan424@gmail.com>
2026-09-07 20:51:03 +0300

    Кортежи с необязатеnьными эnементам

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,11 @@ function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 let hat = ["Hat", 100];
-let gloves = ["Gloves", 75];
-let products = [["Hat", 100], ["Gloves", 75]];
-let tupleUnion = [true, false, hat, ...products];
-tupleUnion.forEach((elem) => {
-    if (elem instanceof Array) {
-        let [str, num] = elem;
-        console.log(`Name: ${str}`);
-        console.log(`Price: ${num.toFixed(2)}`);
+let gloves = ["Gloves", 75, 10];
+[hat, gloves].forEach(tuple => {
+    let [name, price, taxRate] = tuple;
+    if (taxRate != undefined) {
+        price += price * (taxRate / 100);
     }
-    else if (typeof elem === "boolean") {
-        console.log(`Boolean Value: ${elem}`);
-    }
+    writePrice(name, price);
 });
