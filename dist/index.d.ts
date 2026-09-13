4134ee004fd6f6db368da8d121b75250c4621baa
Ilya <ilieturcan424@gmail.com>
2026-09-13 21:06:49 +0300

    Объединение свойств с различными типами

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,16 +2,13 @@ type Person = {
     id: string;
     name: string;
     city: string;
+    contact: number;
 };
 type Employee = {
     id: string;
     company: string;
     dept: string;
+    contact: string;
 };
 type EmployedPerson = Person & Employee;
-declare function correlateData(peopleData: Person[], staff: Employee[]): EmployedPerson[];
-declare let people: Person[];
-declare let employees: Employee[];
-declare let dataItems: EmployedPerson[];
-declare function writePerson(per: Person): void;
-declare function writeEmployee(emp: Employee): void;
+declare let typeTest: never;
