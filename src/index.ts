7a519814a38826c0b394af75fe365b6cfbc8273a
Ilya <ilieturcan424@gmail.com>
2026-09-15 20:53:04 +0300

    Управление доступом с помощью ключевых слов

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ type Person = {
 };
 
 class Employee {
-    id: string;
-    name: string;
-    dept: string;
-    city: string;
+    public id: string;
+    public name: string;
+    private dept: string;
+    public city: string;
 
     constructor(id: string, name: string, dept: string, city: string) {
         this.id = id;
@@ -23,6 +23,7 @@ class Employee {
 }
  
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
+salesEmployee.writeDept();
 
 let data: (Person | Employee)[] = [
     { id: "bsmith", name: "Bob Smith", city: "London" },
