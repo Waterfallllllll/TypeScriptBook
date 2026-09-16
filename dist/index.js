441c744f57bd8b6b9f6c1cb31aebbd2f8b621fbb
Ilya <ilieturcan424@gmail.com>
2026-09-16 20:16:02 +0300

    Определение аксессоров

--- dist/index.js
+++ dist/index.js
@@ -12,20 +12,15 @@ class Employee {
     writeDept() {
         console.log(`${this.name} works in ${this.dept}`);
     }
+    get location() {
+        return this.city;
+    }
+    set location(newCity) {
+        this.city = newCity;
+    }
 }
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 salesEmployee.writeDept();
-let data = [
-    { id: "bsmith", name: "Bob Smith", city: "London" },
-    { id: "ajones", name: "Alice Jones", city: "Paris" },
-    { id: "dpeters", name: "Dora Peters", city: "New York" },
-    salesEmployee,
-];
-data.forEach((item) => {
-    if (item instanceof Employee) {
-        item.writeDept();
-    }
-    else {
-        console.log(`${item.id} ${item.name}, ${item.city}`);
-    }
-});
+console.log(`Location: ${salesEmployee.location}`);
+salesEmployee.location = "London";
+console.log(`Location: ${salesEmployee.location}`);
--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,28 @@ type Person = {
 };
 
 class Employee {
+    private city: string;
 
-    constructor(public readonly id: string, public name: string, private dept: string, public city: string) {
+    constructor(public readonly id: string, public name: string, private dept: string, city: string) {
+        this.city = city;
     }
 
     writeDept() {
         console.log(`${this.name} works in ${this.dept}`);
     }
+
+    get location() {
+        return this.city;
+    }
+
+    set location(newCity) {
+        this.city = newCity;
+    }
 }
  
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 salesEmployee.writeDept();
+console.log(`Location: ${salesEmployee.location}`);
+salesEmployee.location = "London";
+console.log(`Location: ${salesEmployee.location}`);
 
-let data: (Person | Employee)[] = [
-    { id: "bsmith", name: "Bob Smith", city: "London" },
-    { id: "ajones", name: "Alice Jones", city: "Paris" },
-    { id: "dpeters", name: "Dora Peters", city: "New York" },
-    salesEmployee,
-];
-
-data.forEach((item) => {
-    if (item instanceof Employee) {
-        item.writeDept();
-    } else {
-        console.log(`${item.id} ${item.name}, ${item.city}`);
-    }
-});
