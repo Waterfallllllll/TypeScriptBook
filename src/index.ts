f1761028831e28b0f8cb018006f0da1baaf36a3b
Ilya <ilieturcan424@gmail.com>
2026-09-17 20:48:56 +0300

    Необязательные свойства и методы интерфейса

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 interface Person {
     name: string;
     getDetails(): string;
-}
-
-interface DogOwner extends Person {
-    dogName: string;
-    getDogDetails(): string;
+    dogName?: string;
+    getDogDetails?(): string;
 }
 
 class Employee implements Person {
@@ -16,7 +13,7 @@ class Employee implements Person {
     }
 }
 
-class Customer implements DogOwner {
+class Customer implements Person {
     constructor(public readonly id: string, public name: string, public city: string, public creditLimit: number, public dogName: string) {}
 
     getDetails(){
@@ -30,9 +27,11 @@ class Customer implements DogOwner {
 
 let alice = new Customer("ajones", "Alice Jones", "London", 500, "Fido");
 
-let dogOwners: DogOwner[] = [alice];
-dogOwners.forEach(item => console.log(item.getDogDetails()));
-
 let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"), alice];
-data.forEach(item => console.log(item.getDetails()));
+data.forEach(item => {
+    console.log(item.getDetails());
+    if (item.getDogDetails) {
+        console.log(item.getDogDetails());
+    }
+});
 
