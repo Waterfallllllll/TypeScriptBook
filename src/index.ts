1326010ce02013dff908b349d028c4d43c8b678c
Ilya <ilieturcan424@gmail.com>
2026-09-17 21:21:06 +0300

    Опредеnение реаnизации абстрактноrо интерфейса

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,36 @@ interface Person {
     getDogDetails?(): string;
 }
 
-class Employee implements Person {
-    constructor(public readonly id: string, public name: string, private dept: string, public city: string) {}
-
-    getDetails() {
-        return `${this.name} works in ${this.dept}`;
-    }
-}
-
-class Customer implements Person {
-    constructor(public readonly id: string, public name: string, public city: string, public creditLimit: number, public dogName: string) {}
-
-    getDetails(){
-        return `${this.name} has ${this.creditLimit} limit`;
-    }
+abstract class AbstractDogOwner implements Person {
+    abstract name: string;
+    abstract dogName?: string;
+    abstract getDetails(): string;
 
     getDogDetails(): string {
-        return `${this.name} has a dog named ${this.dogName}`;
+        if (this.dogName) {
+            return `${this.name} has a dog called ${this.dogName}`;
+        }
     }
 }
 
-let alice = new Customer("ajones", "Alice Jones", "London", 500, "Fido");
-
-let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"), alice];
-data.forEach(item => {
-    console.log(item.getDetails());
-    if (item.getDogDetails) {
-        console.log(item.getDogDetails());
+class DogOwningCustomer extends AbstractDogOwner {
+    constructor(
+        public readonly id: string,
+        public name: string,
+        public city: string,
+        public creditLimit: number,
+        public dogName: string,
+    ) {
+        super();
     }
-});
 
+    getDetails() {
+        return `${this.name} has ${this.creditLimit} limit`;
+    }
+}
+
+let alice = new DogOwningCustomer("ajones", "Alice Jones", "London", 500, "Fido");
+
+if (alice.getDogDetails) {
+    console.log(alice.getDogDetails());
+}
