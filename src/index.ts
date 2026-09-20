87d04ff7afc1e8dcfafe126db1dfcf82fc5a673a
Ilya <ilieturcan424@gmail.com>
2026-09-20 20:50:47 +0300

    ПРОБЛЕМЫ, РЕШАЕМЫЕ ОБОБЩЕННЫМИ ТИПАМИ

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
-interface Product {
-    name: string;
-    price: number;
-}
+import { Person, Product } from "./dataTypes.js";
 
-class SportsProduct implements Product {
-    constructor(
-        public name: string,
-        public category: string,
-        public price: number,
-    ) {}
-}
+let people = [
+    new Person("Bob Smith", "London"),
+    new Person("Dora Peters", "New York"),
+];
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 
-class ProductGroup {
-    constructor(...initialProducts: [string, Product][]) {
-        initialProducts.forEach((p) => (this[p[0]] = p[1]));
+class PeopleCollection {
+    private items: Person[] = [];
+    constructor(initialItems: Person[]) {
+        this.items.push(...initialItems);
     }
 
-    [propertyName: string]: Product;
+    add(newItem: Person) {
+        this.items.push(newItem);
+    }
+
+    getNames(): string[] {
+        return this.items.map(item => item.name);
+    }
+
+    getItem(index: number): Person {
+        return this.items[index]!;
+    }
 }
 
-let group = new ProductGroup([
-    "shoes",
-    new SportsProduct("Shoes", "Running", 90.5),
-]);
-group.hat = new SportsProduct("Hat", "Skiing", 20);
+let peopleData = new PeopleCollection(people);
 
-if (group.hat && group.boots) {
-    let total = group.hat.price + group.boots.price;
-    console.log(`Total: ${total}`);
-}
-
-let total = group.hat.price + (group.boots?.price ?? 0);
-console.log(`Total: ${total}`);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
