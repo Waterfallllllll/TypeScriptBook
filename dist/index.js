dcfa640c9973af9d74eeeba529ae3e3a82de4697
Ilya <ilieturcan424@gmail.com>
2026-09-22 21:43:47 +0300

    Защита обобщенных типов

--- dist/index.js
+++ dist/index.js
@@ -11,31 +11,13 @@ class DataCollection {
     constructor(initialItems) {
         this.items.push(...initialItems);
     }
-    collate(targetData, itemProp, targetProp) {
-        let results = [];
-        this.items.forEach(item => {
-            let match = targetData.find(d => d[targetProp] === item[itemProp]);
-            if (match !== undefined) {
-                results.push({ ...match, ...item });
-            }
-        });
-        return results;
+    filter(predicate) {
+        return this.items.filter(item => predicate(item));
     }
 }
-class SearchableCollection extends DataCollection {
-    constructor(initialItems) {
-        super(initialItems);
-    }
-    find(searchTerm) {
-        return this.items.filter(item => {
-            if (item instanceof Employee) {
-                return item.name === searchTerm || item.role === searchTerm;
-            }
-            else if (item instanceof Person) {
-                return item.name === searchTerm || item.city === searchTerm;
-            }
-        });
-    }
+let mixedData = new DataCollection([...people, ...products]);
+function isProduct(target) {
+    return target instanceof Product;
 }
-let employeeData = new SearchableCollection(employees);
-employeeData.find("Sales").forEach(e => console.log(`Employee ${e.name}, ${e.role}`));
+let filteredProducts = mixedData.filter(isProduct);
+filteredProducts.forEach(p => console.log(`Product: ${p.name}, ${p.price} `));
