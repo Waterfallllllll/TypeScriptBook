abc7af3c4425f8ce0defff34bb00699109cef638
Ilya <ilieturcan424@gmail.com>
2026-09-24 20:40:19 +0300

    Создание итерируемоrо класса

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 
 type shapeType = { name: string };
 
-class Collection<T extends shapeType> {
+class Collection<T extends shapeType> implements Iterable<T> {
     private items: Map<string, T>;
 
     constructor(initialItems: T[] = []) {
@@ -24,11 +24,12 @@ class Collection<T extends shapeType> {
         return this.items.size;
     }
 
-    values(): IterableIterator<T> {
+    [Symbol.iterator](): Iterator<T> {
         return this.items.values();
     }
 }
 
 let productCollection: Collection<Product> = new Collection<Product>(products);
 console.log(`There are ${productCollection.count} products`);
-[...productCollection.values()].forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
+
+[...productCollection].forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
