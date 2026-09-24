e4b14ef61380faca73e86a62aaa101c3c7ad9bf6
Ilya <ilieturcan424@gmail.com>
2026-09-24 21:27:40 +0300

    Использование индексного типа в классе collection в файле index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,27 @@
-import { Product, Employee } from "./dataTypes.js";
-function getValue(item, keyname) {
-    return item[keyname];
+import { Product } from "./dataTypes.js";
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+class Collection {
+    propertyName;
+    items;
+    constructor(initialItems = [], propertyName) {
+        this.propertyName = propertyName;
+        this.items = new Map();
+        this.add(...initialItems);
+    }
+    add(...newItems) {
+        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
+    }
+    get(key) {
+        return this.items.get(key);
+    }
+    get count() {
+        return this.items.size;
+    }
+    [Symbol.iterator]() {
+        return this.items.values();
+    }
 }
-let p = new Product("Running Shoes", 100);
-console.log(getValue(p, "name"));
-console.log(getValue(p, "price"));
-let e = new Employee("Bob Smith", "Sales");
-console.log(getValue(e, "name"));
-console.log(getValue(e, "role"));
+let productCollection = new Collection(products, "name");
+console.log(`There are ${productCollection.count} products`);
+let itemByKey = productCollection.get("Hat");
+console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
