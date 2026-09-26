7d1a991112ce485387cb2b5ac852008f69fb1656
Ilya <ilieturcan424@gmail.com>
2026-09-26 22:02:12 +0300

    УСЛОВНЫЕ ТИПЫ

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,4 @@
-let p1 = { name: "Bob", city: "London" };
-let p2 = {
-    name: "Alice",
-    city: "Paris",
-};
-console.log(`Custom mapped type: ${p1.name}, ${p1.city}`);
-console.log(`Built-in mapped type: ${p2.name}, ${p2.city}`);
+let firstVal = "String Value";
+let secondVal = 100;
+let mismatchCheck = "String Value";
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { City, Person, Product, Employee } from "./dataTypes.js";
 
-type CustomMapped<K extends keyof any, T> = {
-    [P in K]: T;
-};
+type resultType<T extends boolean> = T extends true ? string : number;
 
-let p1: CustomMapped<"name" | "city", string> = { name: "Bob", city: "London" };
-let p2: Record<"name" | "city", string> = {
-    name: "Alice",
-    city: "Paris",
-};
-console.log(`Custom mapped type: ${p1.name}, ${p1.city}`);
-console.log(`Built-in mapped type: ${p2.name}, ${p2.city}`);
+let firstVal: resultType<true> = "String Value";
+let secondVal: resultType<false> = 100;
+
+let mismatchCheck: resultType<false> = "String Value";
